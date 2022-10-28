2f6fcaa509627d109197ca097cc52402720e5de1
IntoTheVoid07 <brittpierce07@gmail.com>
2022-10-28 12:58:22 -0600

    Adding step 1 stuff

--- project/pid_controller/pid_controller.cpp
+++ project/pid_controller/pid_controller.cpp
@@ -5,40 +5,57 @@
  **********************************************/
 
 #include "pid_controller.h"
-#include <vector>
 #include <iostream>
 #include <math.h>
+#include <vector>
 
-using namespace std;
-
-PID::PID() {}
-
-PID::~PID() {}
+PID::PID() :
+    tau_p_(0.0),
+    tau_i_(0.0),
+    tau_d_(0.0),
+    output_lim_max_(0.0),
+    output_lim_min_(0.0),
+    diff_cte_(0.0),
+    prev_cte_(0.0),
+    int_cte_(0.0)
+{ }
 
 void PID::Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini) {
-   /**
-   * TODO: Initialize PID coefficients (and errors, if needed)
-   **/
+    tau_p_ = Kpi;
+    tau_i_ = Kii;
+    tau_d_ = Kdi;
+    output_lim_max_ = output_lim_maxi;
+    output_lim_min_ = output_lim_mini;
 }
 
-
 void PID::UpdateError(double cte) {
-   /**
-   * TODO: Update PID errors based on cte.
-   **/
+    if (delta_time_ > 0.0) {
+        diff_cte_ = (cte - prev_cte_) / delta_time_;
+    }
+    else {
+        diff_cte_ = 0.0;
+    }
+
+    int_cte_ += cte;
+    prev_cte_ = cte;
 }
 
 double PID::TotalError() {
-   /**
-   * TODO: Calculate and return the total error
-    * The code should return a value in the interval [output_lim_mini, output_lim_maxi]
-   */
-    double control;
-    return control;
+    // control = (-tau_p * CTE) - (tau_d * (d/dt CTE)) - (tau_i * ∫ CTE)
+    //                 ^                   ^                   ^
+    //           Proportional (P)      Differential (D)     Integral (I)
+    double control = -tau_p_ * prev_cte_ - tau_d_ * diff_cte_ - tau_i_ * int_cte_;
+    return PID::clamp(control, output_lim_min_, output_lim_max_);
 }
 
 double PID::UpdateDeltaTime(double new_delta_time) {
-   /**
-   * TODO: Update the delta time with new value
-   */
+    double prev_delta_time = delta_time_;
+    delta_time_ = new_delta_time;
+    return prev_delta_time; // Uncertain if this is what's expected right now...
+}
+
+template<class T>
+constexpr const T& PID::clamp( const T& x, const T& lo, const T& hi)
+{
+    return std::min(hi, std::max(x, lo));
 }
--- project/pid_controller/pid_controller.h
+++ project/pid_controller/pid_controller.h
@@ -9,56 +9,69 @@
 
 class PID {
 public:
-
-   /**
-   * TODO: Create the PID class
-   **/
-
     /*
-    * Errors
-    */
-
-    /*
-    * Coefficients
-    */
-
-    /*
-    * Output limits
-    */
-  
-    /*
-    * Delta time
-    */
-
-    /*
-    * Constructor
-    */
+     * Constructor
+     */
     PID();
 
     /*
-    * Destructor.
-    */
-    virtual ~PID();
+     * Destructor.
+     */
+    ~PID() = default;
 
     /*
-    * Initialize PID.
-    */
-    void Init(double Kp, double Ki, double Kd, double output_lim_max, double output_lim_min);
+     * Initialize PID.
+     */
+    void Init(double Kpi, double Kii, double Kdi, double output_lim_maxi, double output_lim_mini);
 
     /*
-    * Update the PID error variables given cross track error.
-    */
+     * Update the PID error variables given cross track error.
+     */
     void UpdateError(double cte);
 
     /*
-    * Calculate the total PID error.
-    */
+     * Calculate the total PID error within the bounds of [output_lim_mini, output_lim_maxi]
+     */
     double TotalError();
   
     /*
-    * Update the delta time.
-    */
+     * Update the delta time.
+     */
     double UpdateDeltaTime(double new_delta_time);
+
+    /*
+     * Clamps the value between a minimum and maximum value.
+     * @note: If compiler was C++17 with std::clamp, would use that instead
+     */
+    //
+    template<class T>
+    constexpr const T& clamp(const T& x, const T& lo, const T& hi);
+
+private:
+    /*
+     * Coefficients
+     */
+    double tau_p_;
+    double tau_i_;
+    double tau_d_;
+
+    /*
+     * Output limits
+     */
+    double output_lim_max_;
+    double output_lim_min_;
+
+    /*
+     * Errors
+     */
+    double diff_cte_;
+    double prev_cte_;
+    double int_cte_;
+
+    /*
+     * Delta time
+     */
+    double delta_time_;
 };
 
 #endif //PID_CONTROLLER_H
